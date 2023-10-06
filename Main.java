528df8656f59c1f220b1470cbe5bb232fd72fb3d
lonelypenguin245 <144401532+lonelypenguin245@users.noreply.github.com>
2023-10-06 09:13:13 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DoctorManagementProgram.Manager;
+
+import DoctorManagementProgram.Enity.Doctor;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author HI
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Doctor> ld = new ArrayList<>();
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.addDoctor(ld);
+                    break;
+                case 2:
+                    Manager.updateDoctor(ld);
+                    break;
+                case 3:
+                    Manager.deleteDoctor(ld);
+                    break;
+                case 4:
+                    Manager.searchDoctor(ld);
+                    break;
+                case 5:
+                    return;
+            }
+
+        }
+
+    }
+}
